3815eb2496ea5ffe4639c10c56bebfb97f60fd0b
Ismail ichu <ismailichu2001@gmail.com>
2024-11-12 00:26:36 +0530

    Complited done list and delete list

--- src/App.js
+++ src/App.js
@@ -15,34 +15,54 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setToDos([...toDos, {id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
+        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {toDos.map((obj) => {
           return (
+
             <div className="todo">
               <div className="left">
-                <input onChange={(e)=>{
-                  setToDo(toDos.filter(obj2 =>{
-                    if(obj2.id === obj.id) obj2.status=e.target.checked
-                    return obj2
+                <input onChange={(e) => {
+                  setToDos(toDos.filter(obj2 => {
+                    if (obj2.id === obj.id) {
+                      obj2.status = e.target.checked
+                    }
+                      return obj2
                   }))
                 }} value={obj.status} type="checkbox" name="" id="" />
                 <p>{obj.text}</p>
               </div>
               <div className="right">
-                <i className="fas fa-times"></i>
+                <i onClick={(e) => {
+                  setToDos(toDos.filter(obj2 => {
+                    if (obj2.id !== obj.id) {
+                      return obj2
+                    }
+
+                  }))
+                }} className="fas fa-times"></i>
               </div>
             </div>)
-                  })}
-          </div>
-          <div>
-            {toDos.map((obj)=>{
-              if(obj.status) {
-                return (<h1>{obj.text}</h1>)
-              }
-            })}
-          </div>
+        })}
+      </div>
+      <div className="donelist">
+        <h1>Done list</h1>
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return (
+              <div>
+                <div className="todos">
+                  <div className="todo">
+                    <p className="done">{obj.text}</p>
+                    <div className="right">
+                    </div>
+                  </div>
+                </div>
+              </div>)
+          }
+        })}
+      </div>
     </div>
   );
 }
